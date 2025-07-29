d8297defc7304c6dcd3e80c36764770e09978a5c
rishit314 <164995636+rishit314@users.noreply.github.com>
2025-07-29 22:35:59 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Cube-Portfolio/', // 👈 use your repo name here
   plugins: [react()],
 })
